a86e16fde2cd0bb181ac6960a43149ff26bfd526
Felix Jie Rong <felixr@worldmanager.com>
2014-10-31 17:06:57 +1100

    Prepare js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+Reveal.initialize({
+    controls: false,
+    progress: true,
+    history: true,
+    center: true,
+
+    theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
+    transition: Reveal.getQueryHash().transition || 'none', // default/cube/page/concave/zoom/linear/none
+//    backgroundTransition: 'none',
+
+    // Optional libraries used to extend on reveal.js
+    dependencies: [
+        { src: 'bower_components/reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+        { src: 'bower_components/reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
+        { src: 'bower_components/reveal.js/plugin/zoom-js/zoom.js', condition: function() { return !!document.body.classList; } }
+    ]
+
+});
